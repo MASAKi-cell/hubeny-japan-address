217d79e9d27e999ffed01e818a937797eb41aea6
masaki takahashi <bje35149@gmail.com>
2025-09-14 06:52:55 +0900

    ADD： typeの追加

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,6 +1,7 @@
 export type Geocode = { lat: number; lon: number };
 
 export type Ellipsoid = "GRS80" | "WGS84";
+export type Entry<T> = { value: T; expiresAt: number };
 
 export type GeoType = {
   geometry: { coordinates: number[]; type: string };
