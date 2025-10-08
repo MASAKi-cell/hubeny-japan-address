37d2e1775be3d38edac2237ad8ddd89fb603b306
masaki takahashi <bje35149@gmail.com>
2025-10-08 23:21:09 +0900

    ADD：indexのファイル先を移動

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { useHubenyDistance } from "./src/hocks/useHubenyDistance";
-export { getDistance } from "./src/core/getDistance";
-export type { Geocode, Ellipsoid, GeoType } from "./src/types/type";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { useHubenyDistance } from "./hocks/useHubenyDistance";
+export { getDistance } from "./core/getDistance";
+export type { Geocode, Ellipsoid, GeoType } from "./types/type";
